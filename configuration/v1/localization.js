f656d3281939e51459e210e6b4e62b92430ece33
Aravind <aravind@applab.qa>
2021-04-13 13:02:31 +0530

    initial commit

---
+++ configuration/v1/localization.js
@@ -0,0 +1,393 @@
+export const translations = {
+    en: {
+        //login
+        "login": "log in",
+        "sign_in_description": `We Will Send You A Verification Code ${"\n"}On This Mobile Number`,
+        "don't_have_account": "Don't  have an Account? ",
+        "continue_as_guest": "Continue as Guest",
+        "sign_up": "sign up",
+        "mobile_number": "Mobile Number",
+        "phone_number": "Phone Number",
+        "send": "send",
+
+        //otp
+        "enter_otp": "enter otp",
+        "enter_verification_code": `Enter 5 digit verification code sent to ${"\n"}your Number`,
+        "change_phone_number": "Change Phone Number ?",
+        "send_again": "Send Again ?",
+
+        // signup
+        "do_you_have_account": "Do you have an Account ?",
+        "sign_in": "sign in",
+        "full_name": "Full Name",
+        "last_name": "Last Name",
+        "email": "Email",
+        "qid": "Qid",
+
+        // signup validation
+        "phoneNumber_is_required": "Phone number is required",
+        "phoneNumber_is_invalid": "Invalid Phone number",
+        "fullName_is_required": "FullName is Required",
+        "lastName_is_required": "LastName is Required",
+        "email_is_required": "Email is Required",
+        "email_invalid": "Invalid email",
+        "Qid_is_required": "Qid is Required",
+        "Qid_is_invalid": "Qid is Invalid",
+        "invalid_otp": "Invalid OTP",
+
+        //home screen
+        "welcome_to_vip": `Welcome To${"\n"}Vip-974`,
+
+
+        //beach list
+        "beach": "beach",
+        "qar/hour": "QAR/hour",
+        "details": "Details",
+        "pick_up_location": "Pick up Location",
+        "rent_now": "rent now",
+        "proceed_to_payment": "proceed to payment",
+
+
+
+        //choose category screen
+        "choose_terrain": "choose terrain",
+        "type_of_vehicle_you_want": "Please specify the type of vehicles you want, desert or beach",
+        "rent": "rent",
+        "services": "services",
+        "build": "build",
+        "view_cabanas": "view cabanas",
+        "type_of_service_you_want": "Please choose the type of service you want",
+        "choose_build_type": "Please choose the build type, Standard or Custom",
+        "choose_type": "choose type",
+        "choose_service": "Choose Service",
+        "desert": "Desert",
+        "standard": "Standard",
+        "custom": "Custom",
+
+        //choose date screen
+        "select_hours": "Select Hours",
+        "select_time": "Select Time",
+        "submit": "Submit",
+        "washing": "Washing",
+        "towing": "Towing",
+        "warning": "Warning",
+        "choose_the_hours": "Please Choose AtLeast One Hour",
+        "choose_the_day": "Please Choose AtLeast a Day",
+        "choose_time_slot": "Please Choose A TimeSlot",
+        "selected_hours": "Selected Hours",
+
+
+        //summary
+        "time_slot": "time slot",
+        'payments': "payments",
+        "no_items_selected": "No Items Selected",
+        "drinks_box": "drinks box",
+        "home_businesses": "Home Businesses",
+        "done": "done",
+        "items_selected": "Items Selected",
+
+        //servicesList
+        "qar": "qar",
+
+        //size screen
+        "size": "size",
+        "cabana_size": "Cabana Size",
+        "please_enter_the_cubicle_area": `Please enter the cubicle area to be${"\n"}washed in square meters`,
+        "next": "next",
+        "m": "m",
+        "cabana_size_is_required": "cabana size is required",
+        "length": "Length",
+        "width": "Width",
+        "length_is_required": "length is required",
+        "width_is_required": "width is required",
+
+
+
+        //cabana
+        "cabanas": "cabanas",
+        "starting_price": "Starting Price",
+        "features": "features",
+        "location": "location",
+        "pay_now": "pay now",
+        "search": "Search",
+        "request_now": "Request Now",
+
+        // service summary
+        "enter_location": "enter location",
+        "summary": "summary",
+        "fee": "fee",
+        "total": "total",
+        "proceed_to_payment": "proceed to payment",
+
+        // choose location
+        "choose_location": "choose location",
+
+        //locations screen
+        "view_services": "view services",
+        "pick_up": "pickup",
+        "drop_off": "dropoff",
+        "locations": "locations",
+
+        //profileScreen
+        "my_profile": "my profile",
+
+        //edit profile
+        "edit_profile": "edit profile",
+        "save": "save",
+
+        //drawer
+        "home": "Home",
+        "my_profile": "My Profile",
+        "about_us": "About Us",
+        "support": "Support",
+        "logout": "Logout",
+        "privacy_policy": "Privacy Policy",
+        "confirm": "Confirm",
+        "logout_message": "Are you sure want to logout",
+        "cancel": "Cancel",
+        "ok": "Okay",
+        "points": "Points",
+        "gold": "Gold",
+
+        // membership
+        "membership": "membership",
+        "points_history": "points history",
+        "benefits": "Benefits",
+        "vip_gold": "VIP-Gold",
+        "expiring_on": "Expiring on",
+
+        // support
+        "purpose_of_contact": "Purpose of Contact",
+        "message": "Message",
+        'get_in_touch': "Get in Touch !",
+        "purpose_is_required": "The Purpose of Contact is Required",
+        "message_is_required": "The Message is Required",
+
+        // my activities
+        "my_activities": "my activities",
+        "booked_ticket": "booked ticket",
+        "scan_barcode": "scan barcode",
+        "please_take_screenshot": `Pleas take a screenshot to Scan this \nQR Code`,
+
+
+        // rent Summary
+        "rental_success": "Rental done Successfully",
+        "rental_success_message": "Thank you. The lease has been successful. \nYou can follow the order from the \nMy Rentals  page",
+        "my_rentals": "My Rentals",
+        "services_success": "Services Done Successfully",
+        "service_success_message": "Thank you. The Service has been successful. \nYou can follow the order from the \nMy Services  page",
+        "my_services": "My Services",
+        "build_success": "Build done Successfully",
+        "build_success_message": "Thank you. The Build has been successful. \nYou can follow the order from the \nMy Builds  page",
+        "my_builds": "My Builds",
+
+        //custom selection screen
+        "length": "length",
+        "width": "width",
+        "add_name": "Add Name",
+        "name_of_customization": " name of customization",
+
+        //general
+        "empty_content": `We are sorry,\nNothing here yet!`,
+        "otp_has_been_sent": "OTP has been sent to this number",
+
+
+    },
+
+    ar: {
+        //login
+        "login": "log in",
+        "sign_in_description": `We Will Send You A Verification Code ${"\n"}On This Mobile Number`,
+        "don't_have_account": "Don't  have an Account? ",
+        "continue_as_guest": "Continue as Guest",
+        "sign_up": "sign up",
+        "mobile_number": "Mobile Number",
+        "phone_number": "Phone Number",
+        "send": "send",
+
+        //otp
+        "enter_otp": "enter otp",
+        "enter_verification_code": `Enter 5 digit verification code sent to ${"\n"}your Number`,
+        "change_phone_number": "Change Phone Number ?",
+        "send_again": "Send Again ?",
+
+        // signup
+        "do_you_have_account": "Do you have an Account ?",
+        "sign_in": "sign in",
+        "full_name": "Full Name",
+        "last_name": "Last Name",
+        "email": "Email",
+        "qid": "Qid",
+
+        // signup validation
+        "phoneNumber_is_required": "Phone number is required",
+        "phoneNumber_is_invalid": "Invalid Phone number",
+        "fullName_is_required": "FullName is Required",
+        "lastName_is_required": "LastName is Required",
+        "email_is_required": "Email is Required",
+        "email_invalid": "Invalid email",
+        "Qid_is_required": "Qid is Required",
+        "Qid_is_invalid": "Qid is Invalid",
+        "invalid_otp": "Invalid OTP",
+
+        //home screen
+        "welcome_to_vip": `Welcome to${"\n"}VIP-974`,
+
+        //beach list
+        "beach": "beach",
+        "qar/hour": "QAR/hour",
+        "details": "Details",
+        "pick_up_location": "Pick up Location",
+        "rent_now": "rent now",
+        "proceed_to_payment": "proceed to payment",
+
+
+        //choose category screen
+        "choose_terrain": "choose terrain",
+        "type_of_vehicle_you_want": "Please specify the type of vehicles you want, desert or beach",
+        "rent": "rent",
+        "services": "services",
+        "build": "build",
+        "view_cabanas": "view cabanas",
+        "type_of_service_you_want": "Please choose the type of service you want",
+        "choose_build_type": "Please choose the build type, Standard or Custom",
+        "choose_type": "choose type",
+        "choose_service": "Choose Service",
+        "desert": "Desert",
+        "standard": "Standard",
+        "custom": "Custom",
+
+        //choose date screen
+        "select_hours": "Select Hours",
+        "select_time": "Select Time",
+        "submit": "Submit",
+        "washing": "Washing",
+        "towing": "Towing",
+        "warning": "Warning",
+        "choose_the_hours": "Please Choose AtLeast One Hour",
+        "choose_the_day": "Please Choose AtLeast a Day You Want To Spend",
+        "choose_time_slot": "Please Choose A TimeSlot",
+        "selected_hours": "Selected Hours",
+
+
+        //summary
+        "time_slot": "time slot",
+        'payments': "payments",
+        "no_items_selected": "No Items Selected",
+        "drinks_box": "drinks box",
+        "home_businesses": "Home Businesses",
+        "done": "done",
+        "items_selected": "Items Selected",
+
+
+        //servicesList
+        "qar": "qar",
+
+        //size screen
+        "size": "size",
+        "cabana_size": "Cabana Size",
+        "please_enter_the_cubicle_area": `Please enter the cubicle area to be${"\n"}washed in square meters`,
+        "next": "next",
+        "m": "m",
+        "cabana_size_is_required": "cabana size is required",
+        "length": "Length",
+        "width": "Width",
+        "length_is_required": "length is required",
+        "width_is_required": "width is required",
+
+
+
+        //cabana
+        "cabanas": "cabanas",
+        "starting_price": "Starting Price",
+        "features": "features",
+        "location": "location",
+        "pay_now": "pay now",
+        "search": "Search",
+        "request_now": "Request Now",
+
+
+        // service summary
+        "enter_location": "enter location",
+        "summary": "summary",
+        "fee": "fee",
+        "total": "total",
+        "proceed_to_payment": "proceed to payment",
+
+
+        // choose location
+        "choose_location": "choose location",
+
+        //locations screen
+        "view_services": "view services",
+        "pick_up": "pickup",
+        "drop_off": "dropoff",
+        "locations": "locations",
+
+        //profileScreen
+        "my_profile": "my profile",
+
+        //edit profile
+        "edit_profile": "edit profile",
+        "save": "save",
+
+
+        //drawer
+        "home": "Home",
+        "my_profile": "My Profile",
+        "about_us": "About Us",
+        "support": "Support",
+        "logout": "Logout",
+        "privacy_policy": "Privacy Policy",
+        "confirm": "Confirm",
+        "logout_message": "Are you sure want to logout",
+        "cancel": "Cancel",
+        "ok": "Okay",
+        "points": "Points",
+        "gold": "Gold",
+
+        // membership
+        "membership": "membership",
+        "points_history": "points history",
+        "benefits": "Benefits",
+        "vip_gold": "VIP-Gold",
+        "expiring_on": "Expiring on",
+
+        // support
+        "purpose_of_contact": "Purpose of Contact",
+        "message": "Message",
+        'get_in_touch': "Get in Touch !",
+        "purpose_is_required": "The Purpose of Contact is Required",
+        "message_is_required": "The Message is Required",
+
+        // my activities
+        "my_activities": "my activities",
+        "booked_ticket": "booked ticket",
+        "scan_barcode": "scan barcode",
+        "please_take_screenshot": `Pleas take a screenshot to Scan this \nQR Code`,
+
+
+        // rent Summary
+        "rental_success": "Rental done Successfully",
+        "rental_success_message": "Thank you. The lease has been successful. \nYou can follow the order from the \nMy Rentals  page",
+        "my_rentals": "My Rentals",
+        "services_success": "Services Done Successfully",
+        "service_success_message": "Thank you. The Service has been successful. \nYou can follow the order from the \nMy Services  page",
+        "my_services": "My Services",
+        "build_success": "Build done Successfully",
+        "build_success_message": "Thank you. The Build has been successful. \nYou can follow the order from the \nMy Builds  page",
+        "my_builds": "My Builds",
+
+        //custom selection screen
+        "length": "length",
+        "width": "width",
+        "add_name": "Add Name",
+        "name_of_customization": " name of customization",
+
+        //general
+        "empty_content": `نحن نعتذر,\nلا يوجد شئ حتى الآن!`,
+        "otp_has_been_sent": "OTP has been sent to this number",
+
+
+    },
+};
